f756346cc2a832b6847424c2b4878297db6ff409
Stewart Christie <stewart.chrst99@gmail.com>
2022-09-29 17:59:59 -0500

    added try and except onto get_current_weather_response

--- current_weather.py
+++ current_weather.py
@@ -11,7 +11,7 @@ a dictionary collects user inputs and replaces the url query
 returns all weather info for that location 
 '''
 
-key = os.environ.get('WEATHER_KEY')
+key = os.environ.get('WEATHER_KEY') #saved into a enviorment variable 
 #print(key)
 #TODO weather description, and 
 # wind speed for every three hour interval,over the next 5 days.
@@ -24,7 +24,7 @@ def main():
         city = input('Enter name of city: ')
         state = input('Enter state initials: ')
         country = 'us'
-        location = f'{city},{state},{country}'
+        location = f'{city},{state},{country}' #user input  saved into one variable 
         query = {'q': {location},'units':'imperial','appid': key}
         print(f'\n FORCAST FOR {city.upper()}, {state.upper()},{country.upper()}\n')
         #get_usa_weather()
@@ -39,7 +39,10 @@ def main():
         print(f'\n FORCAST FOR {city.upper()}, {country.upper()}\n')
 
     data = get_current_weather_response(query)
-    display_forcast(data)
+    if data is None:
+        print('Error, no data. ')
+    else:
+        display_forcast(data)
     
 
     #pprint(data)
@@ -47,28 +50,25 @@ def main():
     
 
 def get_current_weather_response(location_query):
-    #weather_url = f'https://api.openweathermap.org/data/2.5/weather?'
-    forcast_5_url = 'http://api.openweathermap.org/data/2.5/forecast?'
-    data = requests.get(forcast_5_url,params=location_query).json()
-    return data 
+    try:
+        #weather_url = f'https://api.openweathermap.org/data/2.5/weather?'# current weather only 
+        forcast_5_url = 'http://api.openweathermap.org/data/2.5/forecast?'# 5 day long weather forcast
+        data = requests.get(forcast_5_url,params=location_query).json()
+        return data 
+    except:#TODO catch specific exception, read requests
+        print('Error connecting to server.')
+        #Will return None
 
 def display_forcast(data):
-    #pprint(data)
-    list_of_forcasts  = data['list']
-    for forcast in (list_of_forcasts):
-        timestamp = forcast['dt']
-        forcast_date = datetime.fromtimestamp(timestamp)
-        weather_description = forcast['weather'][0]['description']
-        windspeed = forcast['wind']['speed']
-        temp = forcast['main']['temp']
-        forcast_day = forcast_date.day
-        forcast_month = forcast_date.month
-        forcast_year = forcast_date.year
-        
 
-        for i in range (len(list_of_forcasts)):
+        list_of_forcasts  = data['list']
+        for forcast in (list_of_forcasts):
+            timestamp = forcast['dt']
+            forcast_date = datetime.fromtimestamp(timestamp)
+            weather_description = forcast['weather'][0]['description']
+            windspeed = forcast['wind']['speed']
+            temp = forcast['main']['temp']
             print(forcast_date.strftime('%b') + ' ' + forcast_date.strftime('%d') + ', ' + forcast_date.strftime('%Y')) 
-            i += 1
             print('Time: ' + forcast_date.strftime('%I') + ':' + forcast_date.strftime('%M' + ' ' + forcast_date.strftime('%p')))
             print(f' Forcast: {weather_description}\n Temprature: {temp}F \n Windspeed: {windspeed} MPH\n')
         
